be5a1fea7611a7ed2114b1a1eab198837d82baa9
Canh Mai <canhmai2003@gmail.com>
2024-12-12 11:56:02 +0700

    update code

---
+++ GRBL_CNC/src/cpu_map.h
@@ -0,0 +1,21 @@
+#ifndef cpu_map_h
+#define cpu_map_h
+
+
+#ifdef CPU_MAP_ATMEGA328P // (Arduino Uno) Officially supported by Grbl.
+  #include "cpu_map/cpu_map_atmega328p.h"
+#endif
+
+#ifdef CPU_MAP_ATMEGA2560 // (Arduino Mega 2560) Working @EliteEng
+  #include "cpu_map/cpu_map_atmega2560.h"
+#endif
+
+/* 
+#ifdef CPU_MAP_CUSTOM_PROC
+  // For a custom pin map or different processor, copy and edit one of the available cpu
+  // map files and modify it to your needs. Make sure the defined name is also changed in
+  // the config.h file.
+#endif
+*/
+
+#endif
---
+++ GRBL_CNC/src/grbl.h
@@ -0,0 +1,44 @@
+
+
+#ifndef grbl_h
+#define grbl_h
+
+// Grbl versioning system
+#define GRBL_VERSION "0.9j"
+#define GRBL_VERSION_BUILD "20160726"
+
+// Define standard libraries used by Grbl.
+#include <avr/io.h>
+#include <avr/pgmspace.h>
+#include <avr/interrupt.h>
+#include <avr/wdt.h>
+#include <util/delay.h>
+#include <math.h>
+#include <inttypes.h>    
+#include <string.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+// Define the Grbl system include files. NOTE: Do not alter organization.
+#include "config.h"
+#include "nuts_bolts.h"
+#include "settings.h"
+#include "system.h"
+#include "defaults.h"
+#include "cpu_map.h"
+#include "coolant_control.h"
+#include "eeprom.h"
+#include "gcode.h"
+#include "limits.h"
+#include "motion_control.h"
+#include "planner.h"
+#include "print.h"
+#include "probe.h"
+#include "protocol.h"
+#include "report.h"
+#include "serial.h"
+#include "spindle_control.h"
+#include "stepper.h"
+
+#endif
